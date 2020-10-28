bccb76cacd00b914124828264a815d274f9a2e94
tom_glb <goodMorning_glb@atguigu.com>
2020-10-28 21:08:30 +0800

    第一次添加到缓存库

---
+++ git-test-project/src/com/raozheng/git/FirstGit.java
@@ -0,0 +1,20 @@
+package com.raozheng.git;/**
+ * @author RaoZheng
+ * @create 2020-10-28 20:48
+ */
+
+/**
+ * TODO
+ * @version 1.0
+ * @author raozheng
+ * @date 2020-10-28 20:48
+ */
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("第一个Git测试");
+        System.out.println("第一个Git测试");
+        System.out.println("第一个Git测试");
+        System.out.println("第一个Git测试");
+        System.out.println("第一个Git测试");
+    }
+}
